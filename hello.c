486f8199619d36d54b2d8f994960668030a3b350
Uri Nieto <oriol.nieto@gmail.com>
2014-12-02 13:28:55 -0500

    Added the first version of hello world

---
+++ hello.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  Hello World!
+ *
+ *        Version:  1.0
+ *        Created:  12/02/2014 13:27:09
+ *
+ *         Author:  Oriol Nieto (oriol@nyu.edu)
+ *   Organization:  New York University
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
